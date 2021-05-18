71a50d5cc33136cfab49e3956ae36f9d7f512a51
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-18 20:06:00 +0530

    Added: Given 3 Integer find the Maximum

--- src/main/java/com/maxnumber/MaximumNumber.java
+++ src/main/java/com/maxnumber/MaximumNumber.java
@@ -1,5 +1,26 @@
 package com.maxnumber;
 
 public class MaximumNumber {
+    /**
+     * Compares two Integer objects numerically using compareTo Method
+     * compare three number with each other
+     * Returns:
+     * the value 0 if this Integer is equal to the argument Integer
+     * a value less than 0 if this Integer is numerically less than the argument Integer;
+     * and a value greater than 0 if this Integer is numerically greater than the argument Integer
+     */
+    public Integer MaximumNumber(Integer num1, Integer num2, Integer num3) {
+        Integer maxNum=num1;
+        if (num2.compareTo(maxNum)>0){
+            maxNum=num2;
+        }
+        if (num3.compareTo(maxNum)>0){
+            maxNum=num3;
+        }
+        return maxNum;
+    }
 
+    public static void main(String[] args) {
+        System.out.println("!!!Welcome To Maximum Number Program!!!");
+    }
 }
--- src/test/java/com/maxnumber/MaximumNumberTest.java
+++ src/test/java/com/maxnumber/MaximumNumberTest.java
@@ -1,7 +1,41 @@
 package com.maxnumber;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MaximumNumberTest {
-    public static void main(String[] args) {
-        System.out.println("!!!Welcome To Maximum Number Program!!!");
+
+    MaximumNumber maximum = new MaximumNumber(); //creating object of MaximumNumber Class
+    Integer max;
+
+    /**
+     * in this test case checking 1st position number is max or not
+     * if it max then it will return the same
+     */
+    @Test
+    public void givenMaxNumber1stPosition_returnSameNumber() {
+        max = maximum.MaximumNumber(12, 9, 7);
+        Assertions.assertEquals(12, max);
     }
+
+    /**
+     * in this test case checking 2nd position number is max or not
+     * if it max then it will return the same
+     */
+    @Test
+    public void givenMaxNumber2nd_Position_returnSameNumber() {
+        max = maximum.MaximumNumber(12,23,15);
+        Assertions.assertEquals(23, max);
+    }
+
+    /**
+     * in this test case checking 3rd position number is max or not
+     * if it max then it will return the same
+     */
+    @Test
+    public void givenMaxNumber3rd_Position_returnSameNumber(){
+        max=maximum.MaximumNumber(12,23,35);
+        Assertions.assertEquals(35,max);
+    }
+
 }
