4bf9478794a65c6cc329dad710e846c3e559f93f
Pratap Pawar <pratappawar1997@gmail.com>
2021-05-18 19:58:19 +0530

    Added: Welcome to Maximum Number Using Generics problem

---
+++ src/main/java/com/maxnumber/MaximumNumber.java
@@ -0,0 +1,5 @@
+package com.maxnumber;
+
+public class MaximumNumber {
+
+}
---
+++ src/test/java/com/maxnumber/MaximumNumberTest.java
@@ -0,0 +1,7 @@
+package com.maxnumber;
+
+public class MaximumNumberTest {
+    public static void main(String[] args) {
+        System.out.println("!!!Welcome To Maximum Number Program!!!");
+    }
+}
